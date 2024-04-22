ad4a8889c4c00f75e061e26044aa16d93587d239
Jeffrey Mendez <75602684+jeffmendezz98@users.noreply.github.com>
2024-04-22 15:41:13 -0400

    Updated DBHelper.java.
    Implemented addOneRequest and addOneOffer as well as getList methods for testing

--- app/src/main/java/edu/uga/cs/ridesharing/DBHelper.java
+++ app/src/main/java/edu/uga/cs/ridesharing/DBHelper.java
@@ -5,54 +5,86 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-
-import com.firebase.ui.auth.data.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final String USERS_TABLE = "USERS";
-    public static final String REQUESTS_TABLE = "REQUESTS";
-    public static final String OFFERS_TABLE = "OFFERS";
-    public static final String COLUMN_ID = "user_id";
-    public static final String COLUMN_REQUESTS_ID = "requests_id";
-    public static final String COLUMN_OFFERS_ID = "offers_id";
+    private static final String DB_NAME = "ridesharing.db";
+    private static final int DB_VERSION = 1;
+    private static DBHelper helperInstance;
+    public static final String TABLE_USERS = "users";
+    public static final String USER_COLUMN_ID = "user_id";
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_EMAIL = "email";
     public static final String COLUMN_PASSWORD = "password";
     public static final String COLUMN_POINTS = "points";
+
+    public static final String TABLE_REQUESTS = "REQUESTS";
+    public static final String COLUMN_REQUESTS_ID = "requests_id";
+
+    public static final String TABLE_OFFERS = "OFFERS";
+    public static final String COLUMN_OFFERS_ID = "offers_id";
+
     public static final String COLUMN_DATE = "date";
     public static final String COLUMN_DESTINATION = "destination";
-    public static final String COLUMN_USER_ID = "user_id";
 
-    public DBHelper(@Nullable Context context) {
-        super(context, "ridesharing.db", null, 1);
+    private static final String CREATE_USERS =
+            "CREATE TABLE " + TABLE_USERS + " ("
+                    + USER_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COLUMN_NAME + " TEXT, "
+                    + COLUMN_EMAIL + " TEXT, "
+                    + COLUMN_PASSWORD + " TEXT, "
+                    + COLUMN_POINTS + " INTEGER"
+                    + ")";
+
+    private static final String CREATE_REQUESTS =
+            "CREATE TABLE " + TABLE_REQUESTS + " ("
+                    + COLUMN_REQUESTS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COLUMN_DATE + " TEXT, "
+                    + COLUMN_DESTINATION + " TEXT, "
+                    + USER_COLUMN_ID + " INTEGER"
+                    + ")";
+
+    private static final String CREATE_OFFERS =
+            "CREATE TABLE " + TABLE_OFFERS + " ("
+                    + COLUMN_OFFERS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COLUMN_DATE + " TEXT, "
+                    + COLUMN_DESTINATION + " TEXT, "
+                    + USER_COLUMN_ID + " INTEGER"
+                    + ")";
+
+
+    private DBHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    public static synchronized DBHelper getInstance(Context context){
+        if (helperInstance == null){
+            helperInstance = new DBHelper(context.getApplicationContext());
+        }
+        return helperInstance;
     }
 
     //this is called the first time you try to access a database
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createUserTableStatement = "CREATE TABLE " + USERS_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME + " TEXT, " + COLUMN_EMAIL + " TEXT, "
-                + COLUMN_PASSWORD + " TEXT, " + COLUMN_POINTS + " INTEGER )";
-        String createRequestsTableStatement = "CREATE TABLE " + REQUESTS_TABLE + " (" + COLUMN_REQUESTS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_DATE + " TEXT, "
-                + COLUMN_DESTINATION + " TEXT, " + COLUMN_USER_ID + " INTEGER )";
-
-        String createOffersTableStatement = "CREATE TABLE " + OFFERS_TABLE + " (" + COLUMN_OFFERS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_DATE + " TEXT, "
-                + COLUMN_DESTINATION + " TEXT, " + COLUMN_USER_ID + " INTEGER )";
-
-        db.execSQL(createUserTableStatement);
-        db.execSQL(createRequestsTableStatement);
-        db.execSQL(createOffersTableStatement);
+        db.execSQL(CREATE_USERS);
+        db.execSQL(CREATE_REQUESTS);
+        db.execSQL(CREATE_OFFERS);
     }
 
     // this is called everytime the version of the DB changes
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists " + TABLE_USERS);
+        db.execSQL("drop table if exists " + TABLE_OFFERS);
+        db.execSQL("drop table if exists " + TABLE_REQUESTS);
+        onCreate(db);
     }
 
     public boolean addOneUser(UserModel userModel){
@@ -64,19 +96,51 @@ public class DBHelper extends SQLiteOpenHelper {
         cv.put(COLUMN_PASSWORD, userModel.getPassword());
         cv.put(COLUMN_POINTS, userModel.getPoints());
 
-        long insert = db.insert(USERS_TABLE, null, cv);
+        long insert = db.insert(TABLE_USERS, null, cv);
         if(insert == -1){
             return false;
         } else {
             return true;
         }
-    }//addOne
+    }//addOneUser
+
+    public boolean addOneRequest(RequestsModel requestsModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_NAME, requestsModel.getDate());
+        cv.put(COLUMN_EMAIL, requestsModel.getDestination());
+        cv.put(COLUMN_PASSWORD, requestsModel.getUserID());
+
+        long insert = db.insert(TABLE_REQUESTS, null, cv);
+        if(insert == -1){
+            return false;
+        } else {
+            return true;
+        }
+    }//addOneRequest
+
+    public boolean addOneOffer(OffersModel offersModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_NAME, offersModel.getDate());
+        cv.put(COLUMN_EMAIL, offersModel.getDestination());
+        cv.put(COLUMN_PASSWORD, offersModel.getUserID());
+
+        long insert = db.insert(TABLE_OFFERS, null, cv);
+        if(insert == -1){
+            return false;
+        } else {
+            return true;
+        }
+    }//addOneOffer
 
     public List<UserModel> getUserList(){
         List<UserModel> returnList = new ArrayList<>();
 
         //get data from database
-        String queryString = "SELECT * FROM " + USERS_TABLE;
+        String queryString = "SELECT * FROM " + TABLE_USERS;
 
         SQLiteDatabase db = this.getReadableDatabase();
 
@@ -93,6 +157,67 @@ public class DBHelper extends SQLiteOpenHelper {
 
                 UserModel newUser = new UserModel(userID, userName, userEmail, userPassword, userPoints);
                 returnList.add(newUser);
+            } while (cursor.moveToNext());
+        } else {
+            // failure. Did not add anything to the list.
+
+        }
+
+        cursor.close();
+        db.close();
+        return returnList;
+    }//getUserList
+
+    public List<RequestsModel> getRequestsList(){
+        List<RequestsModel> returnList = new ArrayList<>();
+
+        //get data from database
+        String queryString = "SELECT * FROM " + TABLE_REQUESTS;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToFirst()){
+            //loop throughout the cursor (result set) and create new CountryModel object for each row
+            do {
+                int requestID = cursor.getInt(0);
+                String requestDate = cursor.getString(1);
+                String requestDestination = cursor.getString(2);
+                int requestUserID = cursor.getInt(3);
+
+                RequestsModel newRequest = new RequestsModel(requestID, requestDate, requestDestination, requestUserID);
+                returnList.add(newRequest);
+            } while (cursor.moveToNext());
+        } else {
+            // failure. Did not add anything to the list.
+        }
+
+        cursor.close();
+        db.close();
+        return returnList;
+    }//getRequestsList
+
+    public List<OffersModel> getOffersList(){
+        List<OffersModel> returnList = new ArrayList<>();
+
+        //get data from database
+        String queryString = "SELECT * FROM " + TABLE_OFFERS;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToFirst()){
+            //loop throughout the cursor (result set) and create new CountryModel object for each row
+            do {
+                int offerID = cursor.getInt(0);
+                String offerDate = cursor.getString(1);
+                String offerDestination = cursor.getString(2);
+                int offerUserID = cursor.getInt(3);
+
+                OffersModel newOffer = new OffersModel(offerID, offerDate, offerDestination, offerUserID);
+                returnList.add(newOffer);
             } while (cursor.moveToNext());
         } else {
             // failure. Did not add anything to the list.
