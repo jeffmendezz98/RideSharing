4f9541e179427c981c01da6d8b06190b9321db16
Jeffrey Mendez <75602684+jeffmendezz98@users.noreply.github.com>
2024-05-01 08:42:54 -0400

    Updated OffersRideListFragment

--- app/src/main/java/edu/uga/cs/ridesharing/riderdata/OffersRideListFragment.java
+++ app/src/main/java/edu/uga/cs/ridesharing/riderdata/OffersRideListFragment.java
@@ -1,52 +1,91 @@
 package edu.uga.cs.ridesharing.riderdata;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.Nullable;
-
-import edu.uga.cs.ridesharing.DB.DBHelper;
 import edu.uga.cs.ridesharing.DB.OffersModel;
+import edu.uga.cs.ridesharing.DB.RequestsModel;
 import edu.uga.cs.ridesharing.R;
 
 public class OffersRideListFragment extends Fragment {
 
-    private ListView lv_list;
-    private ArrayAdapter offersArrayAdapter;
+    private ViewGroup offersLayout;
+    private List<OffersModel> offersList;
 
-    public OffersRideListFragment(){
-        // Empty Constructor
+    public OffersRideListFragment() {
+        // Required empty public constructor
     }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
-        // Store fragment context on creation
-        Context context = getActivity().getApplicationContext();
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_current_offers, container, false);
 
-        // Inflate Layout
-        View rootView = inflater.inflate(R.layout.fragment_list_offers, container, false);
+        offersLayout = view.findViewById(R.id.offers_layout);
+        offersList = new ArrayList<>();
 
-        // Initialize views
-        lv_list = rootView.findViewById(R.id.lv_list_offers);
+        // Load offers from Firebase
+        loadOffersFromFirebase();
 
+        return view;
+    }
 
-        DBHelper dbHelper = new DBHelper(context);
-        List<OffersModel> list = dbHelper.getOffersList();
-        offersArrayAdapter = new ArrayAdapter<OffersModel>(
-                context, android.R.layout.simple_list_item_1, dbHelper.getOffersList()
-        );
-        lv_list.setAdapter((offersArrayAdapter));
-        return rootView;
+    private void loadOffersFromFirebase() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("offers");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                offersList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    OffersModel offer = snapshot.getValue(OffersModel.class);
+                    offersList.add(offer);
+                }
+                displayRequests();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle database error
+            }
+        });
+    }
+
+    private void displayRequests() {
+        offersLayout.removeAllViews(); // Clear previous views
+
+        for (OffersModel offer : offersList) {
+            View requestView = LayoutInflater.from(getContext()).inflate(R.layout.accept_layout, offersLayout, false);
+
+            // Populate request data into the view
+            TextView nameTextView = requestView.findViewById(R.id.textViewUserId);
+            TextView destinationTextView = requestView.findViewById(R.id.textViewDestination);
+            TextView dateTextView = requestView.findViewById(R.id.textViewDate);
+            Button acceptButton = requestView.findViewById(R.id.buttonAccept);
+
+            nameTextView.setText(String.valueOf(offer.getUserID()));
+            destinationTextView.setText(offer.getDestination());
+            dateTextView.setText(offer.getDate());
+
+            // Set onClickListener for accept button
+            acceptButton.setOnClickListener(v -> {
+                // Handle accept button click event
+            });
+
+            offersLayout.addView(requestView);
+        }
     }
 }
